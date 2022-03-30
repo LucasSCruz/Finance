fab8264c37b97df741fc69b1f26080cabeaf5a34
Lucas <lucasscruz2001@gmail.com>
2022-03-30 11:46:06 -0300

    Adding the controller and creating the front

--- WebFinance.API/WebFinance/Controllers/CadastroController.cs
+++ WebFinance.API/WebFinance/Controllers/CadastroController.cs
@@ -1,18 +1,44 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebFinance.Data;
 using WebFinance.Models;
+using System.Collections.Generic;
 
 namespace WebFinance.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class CadastroController : ControllerBase
     {
-      [HttpGet]
-      public IEnumerable<Pessoa> pessoa()
+        
+        private DataContext _dataContext;
+
+        public void PessoaController(DataContext context)
         {
-            return new List<Pessoa>();
+            _dataContext = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Pessoa>> pessoa()
+        {
+            _dataContext = new DataContext();
+
+            var data = _dataContext.GetPessoas();
+
+
+            return data.ToList();
         }
 
 
+        [HttpPost]
+        public async Task<IActionResult> Cadastro([Bind("Codigo,Nome")] Pessoa pessoa)
+        {
+            _dataContext = new DataContext();
+
+            _dataContext.Pessoas.Add(pessoa);
+
+            return Created("Pessoas", pessoa);
+        }
     }
 }
--- WebFinance.API/WebFinance/Data/DataContext.cs
+++ WebFinance.API/WebFinance/Data/DataContext.cs
@@ -1,21 +1,28 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using WebFinance.Models;
 
 namespace WebFinance.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext()
-        {
+        private readonly DataContext _context;
 
+        public DataContext(DataContext context)
+        {
+            _context = context;
         }
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DataContext()
+        {
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             if (!options.IsConfigured)
             {
-                options.UseSqlServer("A FALLBACK CONNECTION STRING");
+                options.UseSqlServer("data source=LUCASCRUZ\\SQLEXPRESS;Database=Finance;User Id=sa;Password=869256;");
             }
         }
 
@@ -26,5 +33,21 @@ namespace WebFinance.Data
         public DbSet<Pessoa> Pessoas { get; set; }
 
         public DbSet<Financa> Financas { get; set; }
+
+        public IEnumerable<Pessoa> GetPessoas()
+        {
+            return Pessoas;
+        }
+
+        public void CreatePessoas(Pessoa pessoa)
+        {
+            if(pessoa == null)
+            {
+                throw new ArgumentNullException("Não tem dados para cadastro");
+            }
+
+            _context.Pessoas.Add(pessoa);
+
+        }
     }
 }
